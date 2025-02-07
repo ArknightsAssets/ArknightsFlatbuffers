856b9e9b4affe37ffe01150caabf68fcd61ae43a
ashlen <thesadru@gmail.com>
2025-02-07 17:28:57 +0100

    Update yostar files when possible

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Arknights flatbuffers
 
-Arknights flatbuffers copied from [OpenArknightsFBS](https://github.com/MooncellWiki/OpenArknightsFBS) and automatically extended with TW.
+Arknights flatbuffers copied from [OpenArknightsFBS](https://github.com/MooncellWiki/OpenArknightsFBS) and automatically extended with TW and YoStar. 
+If a file could not be decrypted, its FBS schema will not be included.
--- script.sh
+++ script.sh
@@ -19,7 +19,7 @@ assets_url="$(echo $network_urls | jq -r ".hu")/Android/assets/${res_version}"
 mkdir -p "$BUNDLES"
 
 while IFS="," read -r path hash; do
-    if [[ ${path} == *gamedata* ]]; then
+    if [[ ${path} =~ gamedata|anon ]]; then
         formatted_path=$(echo "$path" | sed -e "s|/|_|g" -e "s|#|__|" -e "s|\..*|.dat|g")
         wget -q -c -P "/tmp/akassets" "${assets_url}/${formatted_path}"
         unzip -q -o "/tmp/akassets/${formatted_path}" -d "$BUNDLES"
@@ -98,4 +98,91 @@ done;
 git -C $OAFBS checkout origin/main
 cp -r $TMP/OpenArknightsFBS/FBS cn
 git -C $OAFBS checkout origin/YoStar
-cp -r $TMP/OpenArknightsFBS/FBS yostar
+# cp -r $TMP/OpenArknightsFBS/FBS yostar
+cp -r $TMP/OpenArknightsFBS/FBS yostar-preferred
+
+
+# ===========================================================================
+# yostar: generate arknights flatbuffers schema by bruteforcing previously-known schemas, but take the current schema as authority
+
+
+TMP="/tmp/fbs-yostar"
+rm -rf $TMP ||:
+mkdir -p $TMP
+
+# download files, see https://github.com/ArknightsAssets/ArknightsAssets for reference
+BUNDLES="$TMP/bundles"
+
+network_config_url="https://ak-conf.arknights.jp/config/prod/official/network_config"; # jp because it's earlier than en
+network_config=$(curl -s "$network_config_url" | jq -r ".content")
+network_urls=$(echo $network_config | jq -r ".configs[$(echo $network_config | jq ".funcVer")].network")
+version_url=$(echo $network_urls | jq -r ".hv" | sed "s/{0}/Android/g")
+res_version=$(curl -s "$version_url" | jq -r ".resVersion")
+assets_url="$(echo $network_urls | jq -r ".hu")/Android/assets/${res_version}"
+
+mkdir -p "$BUNDLES"
+
+while IFS="," read -r path hash; do
+    if [[ ${path} =~ gamedata|anon ]]; then
+        formatted_path=$(echo "$path" | sed -e "s|/|_|g" -e "s|#|__|" -e "s|\..*|.dat|g")
+        wget -q -c -P "/tmp/akassets" "${assets_url}/${formatted_path}"
+        unzip -q -o "/tmp/akassets/${formatted_path}" -d "$BUNDLES"
+        rm "/tmp/akassets/${formatted_path}"
+        echo "${path}"
+    fi
+done < <(curl -s "${assets_url}/hot_update_list.json" | jq -r -c '.abInfos[] | "\(.name),\(.hash)"')
+wait
+
+# download pre-requisites
+wget -q -c -O "$TMP/AssetStudioModCLI.zip" "https://github.com/aelurum/AssetStudio/releases/download/v0.18.0/AssetStudioModCLI_net6_linux64.zip"
+unzip -q -d "$TMP/AssetStudioModCLI" "$TMP/AssetStudioModCLI.zip"
+chmod +x $TMP/AssetStudioModCLI/AssetStudioModCLI
+
+wget -q -c -O "$TMP/flatc.zip" "https://github.com/google/flatbuffers/releases/download/v24.3.25/Linux.flatc.binary.g++-13.zip"
+unzip -q -d "$TMP" "$TMP/flatc.zip"
+chmod +x $TMP/flatc
+
+OAFBS=$TMP/OpenArknightsFBS
+git clone https://github.com/MooncellWiki/OpenArknightsFBS $OAFBS
+
+# go through downloaded files and try to parse them with flatc
+$TMP/AssetStudioModCLI/AssetStudioModCLI "$BUNDLES" -t TextAsset -o "$TMP/extracted"
+
+mkdir -p "$TMP/export"
+mkdir -p "$TMP/extracted-cut"
+mkdir -p "$TMP/fb"
+mkdir -p "yostar"
+mkdir -p "output/yostar"
+mkdir -p "output/yostar/levels"
+for path in $(find "$TMP/extracted/assets/torappu/dynamicassets/gamedata" -type f -name "*.bytes"); do
+    cutpath="$TMP/extracted-cut/$(basename $path)"
+    dd if=$path bs=128 skip=1 of=$cutpath 2>/dev/null
+
+    if [[ $path =~ gamedata/(.+_(table|data|const|database))([0-9a-fA-F]{6}) ]]; then
+        name=$(basename "${BASH_REMATCH[1]}")
+        $TMP/flatc -o "output/yostar" "yostar-preferred/$name.fbs" -- "$cutpath" --no-warnings --json --strict-json --natural-utf8 --defaults-json --unknown-json --raw-binary --force-empty 1> /dev/null 2> /dev/null
+        if [[ $? == 0 ]]; then
+            mv "output/yostar/$(basename $cutpath .bytes).json" "output/yostar/$name.json"
+            cp "yostar-preferred/$name.fbs" yostar/$name.fbs
+            echo $name - default
+            continue
+        fi
+
+        git -C $OAFBS rev-list --all --objects -- FBS/$name.fbs | cut -d ' ' -f1 |
+        while read h; do
+            git -C $OAFBS cat-file -p ${h}:FBS/$name.fbs > $TMP/export/$name.$h.fbs;
+            $TMP/flatc -o "output/yostar" "$TMP/export/$name.$h.fbs" -- "$cutpath" --no-warnings --json --strict-json --natural-utf8 --defaults-json --unknown-json --raw-binary --force-empty 1> /dev/null 2> /dev/null
+            if [[ $? == 0 ]]; then
+                mv "output/yostar/$(basename $cutpath .bytes).json" "output/yostar/$name.json"
+                echo "// https://github.com/MooncellWiki/OpenArknightsFBS/commit/$h" | cat - $TMP/export/$name.$h.fbs > yostar/$name.fbs
+                echo $name - $h
+                break
+            fi
+        done    
+    fi
+done
+
+# todo: yostar levels
+cp yostar-preferred/prts___levels.fbs yostar/prts___levels.fbs
+
+rm -r yostar-preferred
